9e5da3a2b5fc048a722b74d2436a8c683686ae3b
Mauricio Rusinque <merc2717@gmail.com>
2023-11-03 00:59:21 -0500

    Modificaciones en el index y en script para optimizar el codigo

--- js/script.js
+++ js/script.js
@@ -7,58 +7,58 @@ const tocarBoton = btn1.addEventListener("click", () => {
     identificarse();
 });
 
+let alumnos = ["Juan", "Ana", "Pedro"];
+let notaFinal = 0;
+let nombreAlumno; // Declarar la variable a nivel global
 
- //Tenemos el listado de alumnos establecido.
+function identificarse() {
+    nombreAlumno = prompt("¡Hola!, por favor ingrese el nombre del alumno a consultar: ");
 
- 
- let alumnos = ["Juan", "Ana", "Pedro", "Maria", "Martin"]; 
- 
- let notaFinal = 0;
-
-
-//Validamos la existencia del alumno en nuestra base de datos mediante una función.
-function identificarse(alumno, nombre) {
-    let nombreAlumno = prompt("¡Hola!, por favor ingrese el nombre del alumno a consultar: ");
-    
-    if (nombreAlumno !== alumnos[0] && nombreAlumno != alumnos[1] && nombreAlumno != alumnos[2] && nombreAlumno != alumnos[3] && nombreAlumno != alumnos[4] || (nombreAlumno === "" && nombreAlumno == Number))  {
-        alert("El nombre del alumno ingresado no existe, verifique la informacion. ", identificarse());
-        
+    if (!alumnos.includes(nombreAlumno) || nombreAlumno === "" || !isNaN(parseFloat(nombreAlumno))) {
+        alert("El nombre del alumno ingresado no existe o es inválido. Verifique la información.");
+        identificarse(); // Llamada recursiva para volver a pedir el nombre
     } else {
-        alert("Es un gusto saludarle,  " + nombreAlumno); 
-    };
-    
+        alert("Es un gusto saludarle,  " + nombreAlumno);
+        notas(); // Llamar a la función de notas después de identificar al alumno
+    }
+}
 
-};
-
-
-
-function notas (){
+function notas() {
     let nota1 = parseFloat(prompt(nombreAlumno + ", por favor ingrese su primera nota de 1 a 10: ."));
-
     let nota2 = parseFloat(prompt(nombreAlumno + ", por favor ingrese su segunda nota de 1 a 10: ."));
-
     let nota3 = parseFloat(prompt(nombreAlumno + ", por favor ingrese su tercera nota de 1 a 10: ."));
 
-    let notaFinal = (nota1 + nota2 + nota3) /3;
-    
-    if((nota1 > 10 || nota1 < 1) || (nota2 > 10 || nota2 < 1) || (nota3 > 10 || nota3 < 1)){
-        alert("Ha ingresado un valor invalido, empiece de nuevo.")
-        return notas();
+    if ((nota1 > 10 || nota1 < 1) || (nota2 > 10 || nota2 < 1) || (nota3 > 10 || nota3 < 1)) {
+        alert("Ha ingresado un valor inválido, por favor empiece de nuevo.");
+        notas(); // Llamada recursiva para volver a ingresar las notas
+    } else {
+        notaFinal = (nota1 + nota2 + nota3) / 3;
 
-    }else if(notaFinal >= 6){
-        alert(nombreAlumno + " el promedio de tus notas es: " + notaFinal + ", felicitaciones, aprobó");
+        if (notaFinal >= 6) {
+            alert(nombreAlumno + " el promedio de tus notas es: " + notaFinal + ", felicitaciones, aprobó.");
+        } else {
+            alert(nombreAlumno + " el promedio de tus notas es: " + notaFinal + ", lo sentimos, reprobó.");
+        }
 
-    }else{
-        alert(nombreAlumno + " el promedio de tus notas es: " + notaFinal + ", lo sentimos, reprobó");
+        // Almacenar los resultados para imprimir al final
+        resultados.push({ nombre: nombreAlumno, notaFinal: notaFinal, estado: notaFinal >= 6 ? "Aprobado" : "Reprobado" });
     }
+}
 
-    // alert(nombreAlumno + " El promedio de tus notas es: " + notaFinal)
+// identificarse(); // Comentado para que no se ejecute automáticamente al cargar la página
 
-};
+// Almacenar los resultados de cada alumno
+let resultados = [];
 
-identificarse(); 
-notas();
+// Llamar identificarse para cada alumno
+for (let i = 0; i < alumnos.length; i++) {
+    identificarse();
+}
 
+// Imprimir resultados al final
+for (let i = 0; i < resultados.length; i++) {
+    alert(`${resultados[i].nombre} - Nota Final: ${resultados[i].notaFinal} - ${resultados[i].estado}`);
+}
 
 
 
