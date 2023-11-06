60f95e7f24468c995b319f9c4854c66f33fe3656
Mauricio Rusinque <merc2717@gmail.com>
2023-11-05 19:32:43 -0500

    Correccion de la logica del codigo en el archivo js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,34 @@
 /* En esta preentrega realizaré un programa para cálcular la 
 nota final de una lista predeterminada de alumnos de una clase y se determinará si aprueban o no.
-Donde calificaremos de 1 a 10, y promedio para aprobar será por igual o mayor a 6.*/
+Donde calificaremos de 1 a 10, y promedio para aprobar será igual o mayor a 6.*/
 
 
 // Almacenar los resultados de cada alumno
 let resultados = [];
 
-
 const btn1 = document.getElementById("btn1");
-const tocarBoton = btn1.addEventListener("click", () => {
+let alumnos = ["Juan", "Ana", "Pedro"];
+let indiceAlumno = 0; // Variable para rastrear el índice del alumno actual.
+
+btn1.addEventListener("click", () => {
     identificarse();
 });
 
-let alumnos = ["Juan", "Ana", "Pedro"];
-let notaFinal = 0;
-let nombreAlumno; // Declarar la variable a nivel global
-
 function identificarse() {
-    nombreAlumno = prompt("¡Hola!, por favor ingrese el nombre del alumno a consultar: ");
+    if (indiceAlumno < alumnos.length) {
+        nombreAlumno = prompt("¡Hola!, por favor ingrese el nombre del alumno a consultar: ");
 
-    if (!alumnos.includes(nombreAlumno) || nombreAlumno === "" || !isNaN(parseFloat(nombreAlumno))) {
-        alert("El nombre del alumno ingresado no existe o es inválido. Verifique la información.");
-        //identificarse();  Llamada recursiva para volver a pedir el nombre
+        if (!alumnos.includes(nombreAlumno) || nombreAlumno === "" || !isNaN(parseFloat(nombreAlumno))) {
+            alert("El nombre del alumno ingresado no existe o es inválido. Verifique la información.");
+        } else if (resultados.some((resultado) => resultado.nombre === nombreAlumno)) {
+            alert("Ya se ingresaron las notas para este alumno. Por favor, elija otro.");
+        } else {
+            alert("Es un gusto saludarle, " + nombreAlumno);
+            notas();
+        }
     } else {
-        alert("Es un gusto saludarle,  " + nombreAlumno);
-        notas(); // Llamar a la función de notas después de identificar al alumno
+        // Ya se ingresaron las notas para todos los alumnos
+        mostrarResultados();
     }
 }
 
@@ -35,9 +39,9 @@ function notas() {
 
     if ((nota1 > 10 || nota1 < 1) || (nota2 > 10 || nota2 < 1) || (nota3 > 10 || nota3 < 1)) {
         alert("Ha ingresado un valor inválido, por favor empiece de nuevo.");
-        notas(); // Llamada recursiva para volver a ingresar las notas
+        notas();
     } else {
-        notaFinal = (nota1 + nota2 + nota3) / 3;
+        let notaFinal = (nota1 + nota2 + nota3) / 3;
 
         if (notaFinal >= 6) {
             alert(nombreAlumno + " el promedio de tus notas es: " + notaFinal + ", felicitaciones, aprobó.");
@@ -45,25 +49,23 @@ function notas() {
             alert(nombreAlumno + " el promedio de tus notas es: " + notaFinal + ", lo sentimos, reprobó.");
         }
 
-        // Almacenar los resultados para imprimir al final
         resultados.push({ nombre: nombreAlumno, notaFinal: notaFinal, estado: notaFinal >= 6 ? "Aprobado" : "Reprobado" });
+
+        // Pasar al siguiente alumno
+        indiceAlumno++;
+        identificarse();
     }
 }
 
-// identificarse(); // Comentado para que no se ejecute automáticamente al cargar la página
+function mostrarResultados() {
+    let mensajeFinal = "Resultados finales:\n";
 
+    for (let i = 0; i < resultados.length; i++) {
+        mensajeFinal += `${resultados[i].nombre} - Nota Final: ${resultados[i].notaFinal} - ${resultados[i].estado}\n`;
+    }
 
-
-// Llamar identificarse para cada alumno
-for (let i = 0; i < alumnos.length; i++) {
-    identificarse();
-}
-
-// Imprimir resultados al final
-for (let i = 0; i < resultados.length; i++) {
-    alert(`${resultados[i].nombre} - Nota Final: ${resultados[i].notaFinal} - ${resultados[i].estado}`);
-}
-
+    alert(mensajeFinal);
+};
 
 
 
